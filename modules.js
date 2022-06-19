6d84501fcacc848d01a65de8402cb7e5eef2543f
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-06-19 14:03:47 +0100

    how requiring modules work :octocat:

--- modules.js
+++ modules.js
@@ -1,2 +1,22 @@
 console.log(arguments);
 console.log(require('module').wrapper);
+
+class Calculator {
+    add(a, b) {
+        return a + b;
+    } 
+
+    subtract(a, b) {
+        return a - b;
+    }
+
+    multiply(a, b) {
+        return a * b;
+    }
+
+    divide(a, b) {
+        return a / b;
+    }
+}
+
+module.exports = Calculator;
